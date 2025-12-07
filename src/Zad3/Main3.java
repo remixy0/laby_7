d2ae6722439f955a67093636ab91accf771c9b1d
Stanislaw Sikora <remixy0@onet.pl>
2025-12-07 17:24:24 +0100

    Zad 3

--- src/Zad3/Main3.java
+++ src/Zad3/Main3.java
@@ -8,15 +8,21 @@ public class Main3 {
         Queue<Customer> queue = new LinkedList<>();
         queue.offer(new Customer("Jack", 5));
         queue.offer(new Customer("Peter", 8));
+        queue.offer(new Customer("Mark", 4));
+        queue.offer(new Customer("Thomas", 12));
         int queueSize = queue.size();
-        int serviceTime = 0;
-        int waitingTime = 0;
+        double serviceTime = 0;
+        double waitingTime = 0;
 
         while(queue.size()>0){
             Customer c = queue.poll();
             System.out.println("Serving for  " + c.name + " service time: " + c.serviceTime);
+            waitingTime += c.waitingTime;
+            serviceTime += c.serviceTime;
             for(Customer customer: queue) customer.waitingTime += c.serviceTime;
         }
+        System.out.println("Avg waiting time: " + waitingTime/queueSize);
+        System.out.println("Avg service time: " + serviceTime/queueSize);
 
 
     }
