17608a6b4ec20fd298bc92c1599e89b2eb6372aa
Stanislaw Sikora <remixy0@onet.pl>
2025-12-07 13:01:52 +0100

    Zad 2 done

--- src/Zad2/Book.java
+++ src/Zad2/Book.java
@@ -6,10 +6,11 @@ public class Book {
     int year;
     boolean borrowed;
 
-    public Book(String title, String author, int year) {
+    public Book(String title, String author, int year, boolean borrowed) {
         this.title = title;
         this.author = author;
         this.year = year;
+        this.borrowed = borrowed;
     }
 
     @Override
--- src/Zad2/Main2.java
+++ src/Zad2/Main2.java
@@ -1,6 +1,7 @@
 package Zad2;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
@@ -9,13 +10,13 @@ public class Main2 {
     List<Book> books = new ArrayList<>();
     HashMap<String,ArrayList<Book>> bookByAuthor = new HashMap<>();
 
-    books.add(new Book("Numbers Don't Lie", "Vaclav Smil", 2020));
-    books.add(new Book("How the World Really Works", "Vaclav Smil", 2022));
-    books.add(new Book("Energy and Civilization", "Vaclav Smil", 2017));
-    books.add(new Book("Clean Code", "Robert C. Martin", 2008));
-    books.add(new Book("Sapiens", "Yuval Noah Harari", 2011));
-    books.add(new Book("The Pragmatic Programmer", "Andrew Hunt", 1999));
-    books.add(new Book("Atomic Habits", "James Clear", 2018));
+    books.add(new Book("Numbers Don't Lie", "Vaclav Smil", 2020, true));
+    books.add(new Book("How the World Really Works", "Vaclav Smil", 2022,false));
+    books.add(new Book("Energy and Civilization", "Vaclav Smil", 2017,false));
+    books.add(new Book("Clean Code", "Robert C. Martin", 2008,true));
+    books.add(new Book("Sapiens", "Yuval Noah Harari", 2011, false));
+    books.add(new Book("The Pragmatic Programmer", "Andrew Hunt", 1999, true));
+    books.add(new Book("Atomic Habits", "James Clear", 2018,false));
 
 
 
@@ -23,9 +24,40 @@ public class Main2 {
 
         bookByAuthor.forEach((author, booksList) -> {
             System.out.println("Author: " + author);
-            System.out.println("Books: ");
-            booksList.stream().forEach(System.out::println);
+            System.out.println("number of books: " + booksList.size());
+
         });
+
+        bookByAuthor.forEach((author, booksList) -> {});
+
+
+//        bookByAuthor.forEach((author, booksList) -> {
+//            System.out.println("Author: " + author);
+//            System.out.println("Books: ");
+//            booksList.stream().forEach(System.out::println);
+//        });
+
+
+        HashMap.Entry<String, ArrayList<Book>> topAuthorEntry = bookByAuthor.entrySet().stream()
+                .max(Comparator.comparingInt(entry -> entry.getValue().size()))
+                .orElse(null);
+
+        System.out.println("");
+
+        if (topAuthorEntry != null) {
+            System.out.println("Top Author: " + topAuthorEntry.getKey());
+            System.out.println("Count: " + topAuthorEntry.getValue().size());
+        }
+
+
+        System.out.println("");
+        System.out.println("Year filter:");
+        int lata = 10;
+        books.stream().filter(book -> book.year < 2025 - lata).forEach(System.out::println);
+
+        System.out.println("");
+        System.out.println("Borrowed books:");
+        books.stream().filter(book -> book.borrowed == true).forEach(System.out::println);
 
 
     }
